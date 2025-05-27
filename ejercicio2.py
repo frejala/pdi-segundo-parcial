96595847443d37dbbef04cacd765136bedb1e469
frejala <fa.rejala03@gmail.com>
2025-05-27 00:18:05 -0300

    ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,24 @@
+from utils import morphological_reconstruction, read_image
+from config import IMAGES_PATH
+import cv2
+import numpy as np
+
+
+def generate_image_B():
+    image = read_image(f"{IMAGES_PATH}/image_A.bmp")
+
+    _, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    binary_image_bool = binary_image > 0                    
+    
+    marker = np.zeros_like(binary_image_bool, dtype=bool)
+    marker[0,  :] = binary_image_bool[0,  :]
+    marker[-1, :] = binary_image_bool[-1, :]
+    marker[:, 0]  = binary_image_bool[:, 0]
+    marker[:, -1] = binary_image_bool[:, -1]
+
+    exterior = morphological_reconstruction(marker, binary_image_bool)
+
+    image_B = binary_image_bool & (~exterior)
+    image_B = image_B.astype(np.uint8) * 255
+    
+    cv2.imwrite(f"{IMAGES_PATH}/image_B.bmp", image_B)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from ejercicio1 import generate_image_A
+from ejercicio2 import generate_image_B
 
 if __name__ == "__main__":
     generate_image_A()
+    generate_image_B()
