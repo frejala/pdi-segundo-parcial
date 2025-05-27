8d7715cfe740d77fc76ccefd82ece16a25397488
frejala <fa.rejala03@gmail.com>
2025-05-27 15:11:52 -0300

    ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -17,38 +17,23 @@ def generate_image_E():
     marker[:, -1] = inverted_image_C_bool[:, -1]
     
     # se rellena el fondo
-    reconstructed_background = morphological_reconstruction(marker, inverted_image_C_bool)
+    reconstruction_inverted = ~morphological_reconstruction(marker, inverted_image_C_bool)
     
-    # invertimos la imagen para obtener el fondo relleno
-    C_filled = ~reconstructed_background
-    
-    # cv2.imwrite("filled.bmp", C_filled.astype(np.uint8) * 255)
-    
-    # aplicamos xor para obtener los agujeros
-    holes = C_filled ^ image_C
-    
-    # invertimos los agujeros para trabajar con ellos
-    holes_inv = ~holes
-    
-    # cv2.imwrite("holes.bmp", holes_inv.astype(np.uint8) * 255)
-    
+    # aplicamos xor para obtener los agujeros e invertimos
+    holes = ~(reconstruction_inverted ^ image_C)
+        
     # creamos un marcador que contiene los bordes de los agujeros
-    holes_marker = np.zeros_like(holes_inv, dtype=bool)
-    holes_marker[0, :] = holes_inv[0, :]
-    holes_marker[-1, :] = holes_inv[-1, :]
-    holes_marker[:, 0] = holes_inv[:, 0]
-    holes_marker[:, -1] = holes_inv[:, -1]
+    holes_marker = np.zeros_like(holes, dtype=bool)
+    holes_marker[0, :] = holes[0, :]
+    holes_marker[-1, :] = holes[-1, :]
+    holes_marker[:, 0] = holes[:, 0]
+    holes_marker[:, -1] = holes[:, -1]
     
     # reconstruimos la imagen de agujeros
-    reconstructed_holes = morphological_reconstruction(holes_marker, holes_inv)
-    
-    # invertimos
-    reconstructed_holes_inv = ~reconstructed_holes
-    
-    # cv2.imwrite(f"recons.bmp", reconstructed_holes_inv.astype(np.uint8) * 255)
-    
+    reconstructed_holes = ~morphological_reconstruction(holes_marker, holes)
+        
     # aplicamos un and para obtener la imagen E
-    imgae_E = holes_inv & reconstructed_holes_inv
+    imgae_E = holes & reconstructed_holes
 
     image_E = imgae_E.astype(np.uint8) * 255
     cv2.imwrite(f"{IMAGES_PATH}/image_E.bmp", image_E)
